4667f3655167e35a65ddb8a25ccf7b7687e09d35
Vishal <vishal.wadhwa@zomato.com>
2020-03-21 15:24:35 +0530

    remove release tag dependency from run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 set -eu -o pipefail;
 # TODO(might never do this): Ctrl-C not working; workaround - stop container manually
 
-RELEASE=$(curl -s "https://api.github.com/repos/phan/phan/releases/latest" | grep 'tag_name' | cut -d'"' -f4)
-
 # polyfill is not user so not mounting cache dir .phan/ast-cache:/tmp/phan-$USER
 # -t to assign tty for colors
-docker run -t --rm -v $PWD:/src --name phan-analyze phan-docker:$RELEASE -j $(nproc) --progress-bar --automatic-fix $@
+docker run -t --rm -v $PWD:/src --name phan-analyze phan-docker:latest -j $(nproc) --progress-bar --automatic-fix $@
